1eb9e751be498c151106b3231181cfa7a0ae404a
Prabhat <anteshkumar114@gmail.com>
2024-04-20 17:48:49 +0530

    adding 4rd time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className="navbar bg-primary text-slate-200 flex justify-center rounded-md sticky top-0 z-10 shadow-lg text-center text-lg font-bold">
-            <a className="btn-ghost text-xl">Pagalchati ka daily Kharcha</a>
+            <a className="btn-ghost text-xl">Note daily Kharcha</a>
         </div>
 
     )
